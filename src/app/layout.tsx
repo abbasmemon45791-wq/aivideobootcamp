8afe5c52ae11993a192806ceea270a4abcf84427
hubbuyerx-byte <hubbuyerx@gmail.com>
2026-08-14 08:10:10 -0700

    Hardcode FB Pixel ID in layout to fix Vercel deploy missing env vars

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,7 +34,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             t.src=v;s=b.getElementsByTagName(e)[0];
             s.parentNode.insertBefore(t,s)}(window, document,'script',
             'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '${process.env.NEXT_PUBLIC_META_PIXEL_ID}');
+            fbq('init', '2170349516868440');
             fbq('track', 'PageView');
           `}
         </Script>
